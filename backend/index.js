e06b9bb5031e303e89aaba8727cc326156230c42
Murali981 <muraliju981@gmail.com>
2025-01-10 10:36:51 +0530

    Updated the cors origins

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,16 @@ mongoose
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://pepper-cloud-mern-assignment-frontend.vercel.app/",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
 // Routes
